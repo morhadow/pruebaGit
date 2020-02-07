8c77fb6831bb3b407399b2c1a198879b18f8a843
Javier <javav12@gmail.com>
2020-02-07 18:44:16 +0100

    Agregada las funciones suma y resta

---
+++ HerenciaGit/src/pruebaGit/Operaciones.java
@@ -0,0 +1,39 @@
+package pruebaGit;
+
+public class Operaciones {
+	private int operadorA;
+	private int operadorB;
+
+	public Operaciones(int operadorA, int operadorB) {
+		this.operadorA = operadorA;
+		this.operadorB = operadorB;
+	}
+
+	public int suma() {
+		return operadorA + operadorB;
+	}
+	
+	public int resta() {
+		return operadorA - operadorB;
+	}
+	
+
+	public int getOperadorA() {
+		return operadorA;
+	}
+
+	public void setOperadorA(int operadorA) {
+		this.operadorA = operadorA;
+	}
+
+	public int getOperadorB() {
+		return operadorB;
+	}
+
+	public void setOperadorB(int operadorB) {
+		this.operadorB = operadorB;
+	}
+
+}
+
+
